4cd0c083882e76625e2903828d72ab36938f5e9f
Eric Du <eric@honovation.com>
2019-05-31 20:36:46 +0800

    upgrade packages

--- fas/main.py
+++ fas/main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from fastapi import FastAPI
 
 from pydantic import BaseModel
@@ -18,7 +20,7 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+    return {"item_id": item_id, "q": q, 'dt': datetime.now()}
 
 
 @app.put("/items/{item_id}")
