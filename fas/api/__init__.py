1e57d291d6037529b42594ccd289f9d86aa89848
Eric Du <eric@honovation.com>
2019-06-12 17:42:15 +0800

    inject db connection to request in middleware

--- fas/api/__init__.py
+++ fas/api/__init__.py
@@ -2,30 +2,35 @@ from dynaconf import settings
 from fastapi import FastAPI
 from starlette.requests import Request
 
+from fas.util.database import DBPool
 from . import organization
-from .db import open_db_connection_pool, close_db_connection_pool, release_db
+
+_pool: DBPool = DBPool(**settings.DB)
 
 app = FastAPI(debug=settings.DEBUG)
 
 
 @app.on_event('startup')
 async def open_database_connection_pool():
-    await open_db_connection_pool()
+    await _pool.open()
 
 
 @app.on_event('shutdown')
 async def close_database_connection_pool():
-    await close_db_connection_pool()
+    await _pool.close()
 
 
 @app.middleware('http')
-async def release_database_connection(request: Request, call_next):
-    request.state.db = None
+async def inject_database_connection_to_request(request: Request, call_next):
     try:
+        request.state.db = await _pool.acquire(acquire_timeout=3, release_timeout=3)
         return await call_next(request)
     finally:
         if request.state.db:
-            await release_db(request.state.db)
+            try:
+                await request.state.db.release()
+            except Exception:
+                pass
 
 
 @app.get('/')
--- fas/api/organization.py
+++ fas/api/organization.py
@@ -1,45 +1,43 @@
 from typing import List
 
-from fastapi import Depends, APIRouter, Body, HTTPException
+from fastapi import APIRouter, Body, HTTPException
+from starlette.requests import Request
 from starlette.status import HTTP_201_CREATED
 
 from fas.model import *
-from fas.util.database import DBClient
-from .db import acquire_db
 
 router = APIRouter()
 
 
 @router.get('/', response_model=List[Organization])
-async def list_organizations_(db: DBClient = Depends(acquire_db)):
-    return await list_organizations(db)
+async def list_organizations_(request: Request):
+    return await list_organizations(request.state.db)
 
 
 @router.post('/', response_model=Organization, status_code=HTTP_201_CREATED)
-async def create_organization_(name: str = Body(..., min_length=2, max_length=32), db: DBClient = Depends(acquire_db)):
-    return await create_organization(db, name)
+async def create_organization_(request: Request, name: str = Body(..., min_length=2, max_length=32)):
+    return await create_organization(request.state.db, name)
 
 
 @router.get('/{id}', response_model=Organization)
-async def get_organization_(id: int, db: DBClient = Depends(acquire_db)):
-    org = await get_organization(db, id)
+async def get_organization_(request: Request, id: int):
+    org = await get_organization(request.state.db, id)
     if not org:
-        raise HTTPException(status_code=404, detail="Organization not found")
+        raise HTTPException(status_code=404, detail='Organization not found')
     return org
 
 
 @router.put('/{id}')
-async def update_organization_(id: int, name: str = Body(..., min_length=2, max_length=32),
-                               db: DBClient = Depends(acquire_db)):
-    rowcount = await update_organization(db, id, name)
+async def update_organization_(request: Request, id: int, name: str = Body(..., min_length=2, max_length=32)):
+    rowcount = await update_organization(request.state.db, id, name)
     if rowcount == 0:
-        raise HTTPException(status_code=404, detail="Organization not found")
+        raise HTTPException(status_code=404, detail='Organization not found')
     return rowcount
 
 
 @router.delete('/{id}')
-async def delete_organization_(id: int, db: DBClient = Depends(acquire_db)):
-    rowcount = await delete_organization(db, id)
+async def delete_organization_(request: Request, id: int):
+    rowcount = await delete_organization(request.state.db, id)
     if rowcount == 0:
-        raise HTTPException(status_code=404, detail="Organization not found")
+        raise HTTPException(status_code=404, detail='Organization not found')
     return rowcount
